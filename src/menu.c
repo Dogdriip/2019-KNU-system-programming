13d6af9331a47da4f6ab902bae5a9aa025b68d6b
gidskql6671 <gidskql6671@naver.com>
2019-11-21 12:42:33 +0000

    menu의 UI 및 기능 구현

--- src/menu.c
+++ src/menu.c
@@ -1,80 +1,119 @@
-#include <stdio.h>
-#include <string.h>
-#include <curses.h>
-
-#define MENU_INTERVAL 5
-#define MENU_TITLE_Y 6
-#define MENU_SELECT_SINGLE_Y 19
-#define MENU_SELECT_MULTI_Y 23
-#define MENU_SELECT_EXIT_Y 27
-
-int screen_col = 0, screen_row = 0;
-
-void init_game();
-void draw_menu();
-int select_menu();
+#include "menu.h"
 
 int main(){
 	int key = 0;
 	initscr();
 	clear();
-	keypad();
+	keypad(stdscr, TRUE);
 
+	init_game();
 	draw_menu();
-	
+
 	while(key != 3){
+		// key) 1: Single, 2: Multi, 3: Exit
 		key = select_menu();
-	
 	}
+
+	end_game();
 	endwin();
 }
 
-void init_game(){
-	getmaxyx(stdscr, screen_row, screen_col); 
+// ECHO를 끔
+void set_noecho_mode(){
+	struct termios ttystate;
+
+	tcgetattr(0, &ttystate);
+	ttystate.c_lflag &= ~ECHO;
+
+	tcsetattr(0, TCSANOW,  &ttystate);
 }
 
+// 터미널 설정을 저장해두고, 복구함
+void tty_mode(int how){
+	static struct termios original_mode;
+	static int original_flags;
+	static int stored = 0;
+
+	if (how == 0){
+		tcgetattr(0,&original_mode);
+		original_flags = fcntl(0, F_GETFL);
+		stored = 1;
+	}
+	else if ( stored ){
+		tcsetattr(0, TCSANOW, &original_mode);
+		original_flags = fcntl(0, F_SETFL, original_flags);
+	}
+}
+
+// program 시작시 초기화
+void init_game(){
+	tty_mode(0);
+	set_noecho_mode();
+
+	curs_set(0);
+}
+
+// program 종료시 초기화
+void end_game(){
+	tty_mode(1);
+}
+
+// 방향키를 통해 메뉴를 선택할 수 있도록 함.
 int select_menu(){
-	int key = 0;
-	int pos = 1;
-	int x[3] = {(screen_col - strlen("1. Single Game")) / 2, (screen_col - strlen("2. Multi Game")) / 2, (screen_col - strlen("3. Exit")) / 2};
+	int key = 0; // 누른 키
+	int pos = 0; // 현재 '->'커서 위치. 범위는 0,1,2.  0은 싱글, 1은 멀티, 2는 Quit. return할 때 1 더해서 리턴
+	int x[3] = {(COLS - strlen("1. Single Game")) / 2, (COLS - strlen("2. Multi Game")) / 2, (COLS - strlen("3. Exit")) / 2};
 	int y[3] = {MENU_SELECT_SINGLE_Y, MENU_SELECT_MULTI_Y, MENU_SELECT_EXIT_Y};
 
-	move(MENU_SELECT_SINGLE_Y, x_1); 
+	while(1){
+		mvprintw(y[pos], x[pos]-3,"->"); 
+		refresh();
 
-	refresh();
-
-	key = getc();
-
-	return pos;
+		key = getch();
+		if (key == KEY_UP){
+			mvprintw(y[pos], x[pos] - 3, "  ");
+			if (pos > 0)
+				pos--;
+		}
+		else if (key == KEY_DOWN){
+			mvprintw(y[pos], x[pos] - 3, "  ");
+			if (pos < 2)
+				pos++;
+		}
+		else if (key == '\n'){
+			mvprintw(y[pos], x[pos] - 3, "  ");
+			return pos + 1;
+		}
+	}
 }
 
-
+// 메인 메뉴를 그림
 void draw_menu(){
-	for(int i = MENU_INTERVAL; i < screen_col - MENU_INTERVAL; i++)
+	for(int i = MENU_INTERVAL; i < COLS - MENU_INTERVAL; i++)
 		mvaddch(MENU_INTERVAL, i, '*');
-	for(int i = MENU_INTERVAL; i < screen_col - MENU_INTERVAL; i++)
-		mvaddch(screen_row - MENU_INTERVAL, i, '*');
-	for(int i = MENU_INTERVAL; i < screen_row - MENU_INTERVAL; i++)
+	for(int i = MENU_INTERVAL; i < COLS - MENU_INTERVAL; i++)
+		mvaddch(LINES - MENU_INTERVAL, i, '*');
+	for(int i = MENU_INTERVAL; i < LINES - MENU_INTERVAL; i++)
 		mvaddch(i, MENU_INTERVAL, '*');
-	for(int i = MENU_INTERVAL; i < screen_row - MENU_INTERVAL; i++)
-		mvaddch(i, screen_col - MENU_INTERVAL, '*');
+	for(int i = MENU_INTERVAL; i < LINES - MENU_INTERVAL; i++)
+		mvaddch(i, COLS - MENU_INTERVAL, '*');
 
 
-	mvprintw(MENU_TITLE_Y, (screen_col - strlen(" /$$$$$$$              /$$     /$$     /$$                 /$$$$$$$$                  /$$                    ")) / 2, " /$$$$$$$              /$$     /$$     /$$                 /$$$$$$$$                  /$$                    ");         
-	mvprintw(MENU_TITLE_Y+1, (screen_col - strlen("| $$__  $$            | $$    | $$    | $$                |__  $$__/                 |__/                    "))/2, "| $$__  $$            | $$    | $$    | $$                |__  $$__/                 |__/                    "); 
-	mvprintw(MENU_TITLE_Y+2, (screen_col - strlen("| $$  \\ $$  /$$$$$$  /$$$$$$ /$$$$$$  | $$  /$$$$$$          | $$ /$$   /$$  /$$$$$$  /$$ /$$$$$$$   /$$$$$$ "))/2, "| $$  \\ $$  /$$$$$$  /$$$$$$ /$$$$$$  | $$  /$$$$$$          | $$ /$$   /$$  /$$$$$$  /$$ /$$$$$$$   /$$$$$$ ");
-	mvprintw(MENU_TITLE_Y+3, (screen_col - strlen("| $$$$$$$  |____  $$|_  $$_/|_  $$_/  | $$ /$$__  $$         | $$| $$  | $$ /$$__  $$| $$| $$__  $$ /$$__  $$"))/2, "| $$$$$$$  |____  $$|_  $$_/|_  $$_/  | $$ /$$__  $$         | $$| $$  | $$ /$$__  $$| $$| $$__  $$ /$$__  $$");
-	mvprintw(MENU_TITLE_Y+4, (screen_col - strlen("| $$__  $$  /$$$$$$$  | $$    | $$    | $$| $$$$$$$$         | $$| $$  | $$| $$  \\ $$| $$| $$  \\ $$| $$  \\ $$")) / 2, "| $$__  $$  /$$$$$$$  | $$    | $$    | $$| $$$$$$$$         | $$| $$  | $$| $$  \\ $$| $$| $$  \\ $$| $$  \\ $$");
-	mvprintw(MENU_TITLE_Y+5, (screen_col - strlen("| $$  \\ $$ /$$__  $$  | $$ /$$| $$ /$$| $$| $$_____/         | $$| $$  | $$| $$  | $$| $$| $$  | $$| $$  | $$"))/2, "| $$  \\ $$ /$$__  $$  | $$ /$$| $$ /$$| $$| $$_____/         | $$| $$  | $$| $$  | $$| $$| $$  | $$| $$  | $$");
-	mvprintw(MENU_TITLE_Y+6, (screen_col - strlen("| $$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$/| $$|  $$$$$$$         | $$|  $$$$$$$| $$$$$$$/| $$| $$  | $$|  $$$$$$$")) /2,"| $$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$/| $$|  $$$$$$$         | $$|  $$$$$$$| $$$$$$$/| $$| $$  | $$|  $$$$$$$");
-	mvprintw(MENU_TITLE_Y+7, (screen_col - strlen("|_______/  \\_______/   \\___/   \\___/  |__/ \\_______/         |__/ \\____  $$| $$____/ |__/|__/  |__/ \\____  $$"))/2, "|_______/  \\_______/   \\___/   \\___/  |__/ \\_______/         |__/ \\____  $$| $$____/ |__/|__/  |__/ \\____  $$");
-	mvprintw(MENU_TITLE_Y+8, (screen_col - strlen("                                                                  /$$  | $$| $$                     /$$  \\ $$"))/2, "                                                                  /$$  | $$| $$                     /$$  \\ $$");
-	mvprintw(MENU_TITLE_Y+9, (screen_col - strlen("                                                                 |  $$$$$$/| $$                    |  $$$$$$/"))/2, "                                                                 |  $$$$$$/| $$                    |  $$$$$$/");
-	mvprintw(MENU_TITLE_Y+10, (screen_col - strlen("                                                                  \\______/ |__/                     \\______/ "))/2, "                                                                  \\______/ |__/                     \\______/ ");
+	mvprintw(MENU_TITLE_Y, (COLS - strlen(" /$$$$$$$              /$$     /$$     /$$                 /$$$$$$$$                  /$$                    ")) / 2, " /$$$$$$$              /$$     /$$     /$$                 /$$$$$$$$                  /$$                    ");         
+	mvprintw(MENU_TITLE_Y+1, (COLS - strlen("| $$__  $$            | $$    | $$    | $$                |__  $$__/                 |__/                    "))/2, "| $$__  $$            | $$    | $$    | $$                |__  $$__/                 |__/                    "); 
+	mvprintw(MENU_TITLE_Y+2, (COLS - strlen("| $$  \\ $$  /$$$$$$  /$$$$$$ /$$$$$$  | $$  /$$$$$$          | $$ /$$   /$$  /$$$$$$  /$$ /$$$$$$$   /$$$$$$ "))/2, "| $$  \\ $$  /$$$$$$  /$$$$$$ /$$$$$$  | $$  /$$$$$$          | $$ /$$   /$$  /$$$$$$  /$$ /$$$$$$$   /$$$$$$ ");
+	mvprintw(MENU_TITLE_Y+3, (COLS - strlen("| $$$$$$$  |____  $$|_  $$_/|_  $$_/  | $$ /$$__  $$         | $$| $$  | $$ /$$__  $$| $$| $$__  $$ /$$__  $$"))/2, "| $$$$$$$  |____  $$|_  $$_/|_  $$_/  | $$ /$$__  $$         | $$| $$  | $$ /$$__  $$| $$| $$__  $$ /$$__  $$");
+	mvprintw(MENU_TITLE_Y+4, (COLS - strlen("| $$__  $$  /$$$$$$$  | $$    | $$    | $$| $$$$$$$$         | $$| $$  | $$| $$  \\ $$| $$| $$  \\ $$| $$  \\ $$")) / 2, "| $$__  $$  /$$$$$$$  | $$    | $$    | $$| $$$$$$$$         | $$| $$  | $$| $$  \\ $$| $$| $$  \\ $$| $$  \\ $$");
+	mvprintw(MENU_TITLE_Y+5, (COLS - strlen("| $$  \\ $$ /$$__  $$  | $$ /$$| $$ /$$| $$| $$_____/         | $$| $$  | $$| $$  | $$| $$| $$  | $$| $$  | $$"))/2, "| $$  \\ $$ /$$__  $$  | $$ /$$| $$ /$$| $$| $$_____/         | $$| $$  | $$| $$  | $$| $$| $$  | $$| $$  | $$");
+	mvprintw(MENU_TITLE_Y+6, (COLS - strlen("| $$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$/| $$|  $$$$$$$         | $$|  $$$$$$$| $$$$$$$/| $$| $$  | $$|  $$$$$$$")) /2,"| $$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$/| $$|  $$$$$$$         | $$|  $$$$$$$| $$$$$$$/| $$| $$  | $$|  $$$$$$$");
+	mvprintw(MENU_TITLE_Y+7, (COLS - strlen("|_______/  \\_______/   \\___/   \\___/  |__/ \\_______/         |__/ \\____  $$| $$____/ |__/|__/  |__/ \\____  $$"))/2, "|_______/  \\_______/   \\___/   \\___/  |__/ \\_______/         |__/ \\____  $$| $$____/ |__/|__/  |__/ \\____  $$");
+	mvprintw(MENU_TITLE_Y+8, (COLS - strlen("                                                                  /$$  | $$| $$                     /$$  \\ $$"))/2, "                                                                  /$$  | $$| $$                     /$$  \\ $$");
+	mvprintw(MENU_TITLE_Y+9, (COLS - strlen("                                                                 |  $$$$$$/| $$                    |  $$$$$$/"))/2, "                                                                 |  $$$$$$/| $$                    |  $$$$$$/");
+	mvprintw(MENU_TITLE_Y+10, (COLS - strlen("                                                                  \\______/ |__/                     \\______/ "))/2, "                                                                  \\______/ |__/                     \\______/ ");
 
-	mvprintw(MENU_SELECT_SINGLE_Y, (screen_col - strlen("1. Single Game")) / 2, "1. Single Game"); 
-	mvprintw(MENU_SELECT_MULTI_Y, (screen_col - strlen("2. Multi Game")) / 2, "2. Multi Game"); 
-	mvprintw(MENU_SELECT_EXIT_Y, (screen_col - strlen("3. Exit")) / 2, "3. Exit"); 
+	mvprintw(MENU_SELECT_SINGLE_Y, (COLS - strlen("1. Single Game")) / 2, "1. Single Game"); 
+	mvprintw(MENU_SELECT_MULTI_Y, (COLS - strlen("2. Multi Game")) / 2, "2. Multi Game"); 
+	mvprintw(MENU_SELECT_EXIT_Y, (COLS - strlen("3. Exit")) / 2, "3. Exit"); 
 
 
 	refresh();
---
+++ src/menu.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdio.h>
+#include <fcntl.h>
+#include <termio.h>
+#include <unistd.h>
+#include <string.h>
+#include <curses.h>
+
+#define MENU_INTERVAL 5 // 메뉴 테두리가 스크린 가장자리로부터 얼마나 떨어지는가
+#define MENU_TITLE_Y 6 // 제목의 Y좌표
+#define MENU_SELECT_SINGLE_Y 19 // Single 선택의 Y 좌표
+#define MENU_SELECT_MULTI_Y 23 // Multi 선택의 Y 좌표
+#define MENU_SELECT_EXIT_Y 27 // Exit 선택의 Y 좌표
+
+// ncurses에 정의되어 있는 전역 변수
+// 기본 윈도우 : stdscr
+// Screen Row : LINES 
+// Screen Col : COLS
+
+void set_noecho_mode();
+void tty_mode(int how);
+void init_game();
+void end_game();
+void draw_menu();
+int select_menu();
